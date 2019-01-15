111fdf48637c10757e0da3491e951fc16919ce07
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-01-15 21:00:05 +0100

    .

--- src/[06] setState/App.js
+++ src/[06] setState/App.js
@@ -9,6 +9,20 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   render() {
     return (
       <div>
